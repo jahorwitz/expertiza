4059f292695130a16063bcf3e980aa1ce7dca851
Expertiza Developer <Anjaney>
2020-10-18 18:05:53 -0400

    Test commit

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -1,4 +1,13 @@
 describe 'Tests mailer' do
+
+  it 'this is a test' do
+    #this is a test
+    test = 1 
+    expect(test).to eq(0)
+  end
+
+  it ''
+
   it 'should send email to required email address with proper content ' do
     # Send the email, then test that it got queued
     email = Mailer.sync_message(
@@ -17,4 +26,5 @@ describe 'Tests mailer' do
     expect(email.to[0]).to eq('expertiza.development@gmail.com')
     expect(email.subject).to eq('Test')
   end
+
 end
