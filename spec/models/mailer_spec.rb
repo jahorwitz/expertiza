46c9dbc9992e587981a018985aaa162d0d1a293d
Joshua Horwitz <jhorwitz@Joshuas-MacBook-Pro.local>
2020-10-18 14:48:23 -0600

    added test for suggested_topic_approved_message

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -17,4 +17,22 @@ describe 'Tests mailer' do
     expect(email.to[0]).to eq('expertiza.development@gmail.com')
     expect(email.subject).to eq('Test')
   end
+
+  it 'should send email with a suggested topic is approved' do
+    # Send the email, then test that it got queued
+    email = Mailer.suggested_topic_approved_message(
+      to: 'tluo@ncsu.edu',
+      cc: 'tluo2@ncsu.edu',
+      subject: "Suggested topic 'Test' has been approved",
+      body: {
+        approved_topic_name: 'assignment',
+        proposer: 'User'
+      }
+    ).deliver_now
+
+    expect(email.from[0]).to eq("expertiza.development@gmail.com")
+    expect(email.to[0]).to eq('expertiza.development@gmail.com')
+    expect(email.bcc[0]).to eq('expertiza.development@gmail.com')
+    expect(email.subject).to eq("Suggested topic 'Test' has been approved")
+  end
 end
